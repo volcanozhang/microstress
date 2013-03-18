de6ebf193872016489003c04f4dbb41890c1fcc1
volcanozhang <fzhang.cristal@icloud.com>
2013-03-18 11:31:36 +0100

    Show the subimage of a certain hkl, and return the subimage.

---
+++ hkl_show.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+#from scipy import optimize, stats
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+hklxy, path = np.load('hklxy_peak_20.npy')
+
+offset = 4096
+framedim = (2594, 2774)
+nb_elem = framedim[0]*framedim[1]
+formatdata = np.uint16
+
+f = open(path, 'rb')
+f.seek(offset)
+image = np.fromfile(f, dtype = formatdata, count = nb_elem).reshape(framedim)
+
+print hklxy.keys()
+def show(hkl, rx = 10, ry = 10):
+    cen = hklxy[hkl]
+    int_x, int_y = int(round(cen[1])), int(round(cen[0]))
+    subimage = image[int_x-rx:int_x+rx+1, int_y-rx:int_y+ry+1]
+    plt.imshow(np.log(subimage))
+    plt.show()
+    return subimage
