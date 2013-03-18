be60f15c2116f270e75b7b125bc7ce6f2fec39c4
volcanozhang <fzhang.cristal@icloud.com>
2013-03-18 14:06:14 +0100

    Add another function which returns the pixel of a certain spot.

--- hkl_show.py
+++ hkl_show.py
@@ -14,12 +14,23 @@ formatdata = np.uint16
 f = open(path, 'rb')
 f.seek(offset)
 image = np.fromfile(f, dtype = formatdata, count = nb_elem).reshape(framedim)
+f.close()
 
 print hklxy.keys()
 def show(hkl, rx = 10, ry = 10):
     cen = hklxy[hkl]
     int_x, int_y = int(round(cen[1])), int(round(cen[0]))
-    subimage = image[int_x-rx:int_x+rx+1, int_y-rx:int_y+ry+1]
+    subimage = image[int_x-rx:int_x+rx+1, int_y-ry:int_y+ry+1]
     plt.imshow(np.log(subimage))
     plt.show()
     return subimage
+
+def xys(hkl, rx = 10, ry = 10):
+    cen = hklxy[hkl]
+    int_x, int_y = int(round(cen[1])), int(round(cen[0]))
+    subimage = image[int_x-rx:int_x+rx+1, int_y-ry:int_y+ry+1]
+    xys = []
+    for i in range(int_x-rx, int_x+rx+1):
+        for j in range(int_y-ry, int_y+ry+1):
+            xys.append((i, j))
+    return tuple(xys)
