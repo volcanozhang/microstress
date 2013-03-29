a24142f1c2a08250af2a61c977a9fcd386a52dd2
volcanozhang <fzhang.cristal@icloud.com>
2013-03-29 11:01:40 +0100

    Add the background correction.

--- noise.py
+++ noise.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.ma as ma
 import scipy as sp
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -7,6 +8,8 @@ import pylab
 from math import pow, sqrt, exp, log, sin, cos, pi
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+
+from common import stringint
 
 fig = pylab.figure()
 ax = Axes3D(fig)
@@ -17,29 +20,24 @@ framedim = (2594, 2774)
 images = np.zeros((100, framedim[0], framedim[1]))
 nb_elem = framedim[0] * framedim[1]
 formatdata = np.uint16
-dirpath = '/home/fengguo/Data/21Feb13/Si1g_100N/nomvt/'
-for i in range(10):
-    path = dirpath + 'S1gnomvt_000%i_mar.tiff'%i
+dirpath = '/home/fengguo/Data/Si1g_5N/nomvt/'
+
+bkg = np.load("background.npy")
+for i in range(100):
+    path = dirpath + 'S1gnomvt_%s_mar.tiff'%stringint(i, 4)
     f = open(path, 'rb')
     f.seek(offset)
-    images[i] = np.fromfile(f, dtype = formatdata, count = nb_elem).reshape(framedim)
-    f.close()
-for i in range(10, 100):
-    path = dirpath + 'S1gnomvt_00%i_mar.tiff'%i
-    f = open(path, 'rb')
-    f.seek(offset)
-    images[i] = np.fromfile(f, dtype = formatdata, count = nb_elem).reshape(framedim)
+    images[i] = np.fromfile(f, dtype = formatdata, count = nb_elem).reshape(framedim)+bkg-100
     f.close()
 
 #np.save('21_Si1g_5N_nomvt', images)
-
 mean, std = np.zeros(framedim), np.zeros(framedim)
 for i in range(framedim[0]):
     for j in range(framedim[1]):
         mean[i, j], std[i, j] = images[:, i, j].mean(), images[:, i, j].std()
 
-np.save('21_Si1g_100N_nomvt_mean', mean)
-np.save('21_Si1g_100N_nomvt_std', std)
+np.save('21_Si1g_5N_nomvt_mean', mean)
+np.save('21_Si1g_5N_nomvt_std', std)
 #mean = images[:]
 """
 # for displaying
