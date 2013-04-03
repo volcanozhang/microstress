b84bc67a4f9054c52d466c942f82346f522c25b5
volcanozhang <fzhang.cristal@icloud.com>
2013-04-03 20:06:29 +0200

    Changed the names of some variables.

--- artificial.py
+++ artificial.py
@@ -11,18 +11,18 @@ from matplotlib import cm
 fig = pylab.figure()
 ax = Axes3D(fig)
 
-FWHMx, FWHMy, chi, Imax = 2., 1., 0., 10.
-twotheta, gamma, full2theta, fullgamma = pi/2, pi, pi, 2*pi
-sqcoschi, sin2chi, coef = pow(cos(chi), 2), sin(2*chi), 8*log(2)
-sqsinchi = 1-sqcoschi
+FWHMx, FWHMy, xi, Imax = 2., 1., 0., 10.
+twotheta, chi, full2theta, fullchi = pi/2, pi, pi, 2*pi
+sqcosxi, sin2xi, coef = pow(cos(xi), 2), sin(2*xi), 8*log(2)
+sqsinxi = 1-sqcosxi
 coefx, coefy = coef/pow(FWHMx, 2), coef/pow(FWHMy, 2)
-A, B, C = (sqcoschi*coefx+sqsinchi*coefy)/2, sin2chi*(coefy-coefx)/4, (sqcoschi*coefy+sqsinchi*coefx)/2
+A, B, C = (sqcosxi*coefx+sqsinxi*coefy)/2, sin2xi*(coefy-coefx)/4, (sqcosxi*coefy+sqsinxi*coefx)/2
 
-gaussian = lambda x, y: Imax*exp(-(A*pow(x-twotheta, 2)+2*B*(x-twotheta)*(y-gamma)+C*pow(y-gamma, 2)))
+gaussian = lambda x, y: Imax*exp(-(A*pow(x-twotheta, 2)+2*B*(x-twotheta)*(y-chi)+C*pow(y-chi, 2)))
 
 # for displaying the spot
 numx, numy = 50, 100
-scalex, scaley = full2theta, fullgamma
+scalex, scaley = full2theta, fullchi
 func = gaussian
 x, y = np.mgrid[0:scalex:complex(0,numx),0:scaley:complex(0,numy)]
 
