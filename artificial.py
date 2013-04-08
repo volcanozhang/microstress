41cdd8cb1d41ad978fbb8ec03b1f8445dfe17bf6
volcanozhang <fzhang.cristal@icloud.com>
2013-04-08 16:38:48 +0200

    Artificial spot at different height.

--- artificial.py
+++ artificial.py
@@ -1,26 +1,81 @@
 import numpy as np
+from numpy import random
 import scipy as sp
+from scipy import integrate
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pylab
 
 from math import pow, sqrt, exp, log, sin, cos, pi
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
+#from mpl_toolkits.mplot3d import Axes3D
+#from matplotlib import cm
 
-fig = pylab.figure()
-ax = Axes3D(fig)
+from common import xy2angle, angle2xy
 
-FWHMx, FWHMy, xi, Imax = 2., 1., 0., 10.
-twotheta, chi, full2theta, minchi, maxchi = pi/2, 0, pi, -pi, pi
+D2R = pi/180.
+R2D = 180./pi
+
+#fig = pylab.figure()
+#ax = Axes3D(fig)
+hklfit = np.load('hklfit.npy').item()
+hklxy = np.load('hkl_xy.npy').item()
+
+hkl = hklfit.keys()[0]
+alpha = hklfit[hkl][0]
+x0, y0 = hklxy[hkl]
+
+sizex, sizey = 31, 31
+
+himage, limage = np.zeros((sizex, sizey)), np.zeros((sizex, sizey))
+twoTheta0, Chi0 = xy2angle(x0, y0)
+twotheta0, chi0 = twoTheta0 * D2R, Chi0 * D2R
+
+FWHMx, FWHMy, xi, Imax = 0.003, 0.0028, 0., 1700.
+full2theta, minchi, maxchi = pi, -pi, pi
 sqcosxi, sin2xi, coef = pow(cos(xi), 2), sin(2*xi), 8*log(2)
 sqsinxi = 1-sqcosxi
 coefx, coefy = coef/pow(FWHMx, 2), coef/pow(FWHMy, 2)
 A, B, C = (sqcosxi*coefx+sqsinxi*coefy)/2, sin2xi*(coefy-coefx)/4, (sqcosxi*coefy+sqsinxi*coefx)/2
 
-gaussian = lambda x, y: Imax*exp(-(A*pow(x-twotheta, 2)+2*B*(x-twotheta)*(y-chi)+C*pow(y-chi, 2)))
+gaussian = lambda x, y: Imax*exp(-(A*(x-twotheta0)**2+2*B*(x-twotheta0)*(y-chi0)+C*(y-chi0)**2))
 
+dd = 59.836
+
+def gaussian_xy_low(x, y, dd = 0.5 * dd):
+    cenx, ceny = angle2xy(twoTheta0, Chi0, dd = dd)
+    two_Theta, Chi = xy2angle(x - sizex/2 + cenx, y - sizey/2 + ceny, dd = dd)
+    two_theta, _chi = two_Theta * D2R, Chi * D2R
+    return gaussian(two_theta, _chi)
+def gaussian_xy_high(x, y, dd = dd):
+    cenx, ceny = angle2xy(twoTheta0, Chi0, dd = dd)
+    two_Theta, Chi = xy2angle(x - sizex/2 + cenx, y - sizey/2 + ceny, dd = dd)
+    two_theta, _chi = two_Theta * D2R, Chi * D2R
+    return gaussian(two_theta, _chi)
+es_himage, es_limage = np.zeros((sizex, sizey)), np.zeros((sizex, sizey))
+expe, var = np.load('exp_var.npy')[0: 2]
+for i in range(sizex):
+    for j in range(sizey):
+        lower, upper = lambda x: j, lambda x: j+1
+        es_limage[i, j] = integrate.dblquad(gaussian_xy_low, i, i+1, lower, upper)[0]
+        es_himage[i, j] = integrate.dblquad(gaussian_xy_high, i, i+1, lower, upper)[0]
+np.save('es_limage', es_limage)
+np.save('es_himage', es_himage)
 # for displaying the spot
+"""
+es_image = np.load('es_image.npy')
+images = np.zeros((100, 21, 21))
+for k in range(100):
+    for i in range(21):
+        for j in range(21):
+            esti = es_image[i, j]
+            images[k, i, j] = round(random.normal(esti+expe, sqrt(alpha*esti + var)))
+mean, std = np.zeros((21,21)), np.zeros((21,21))
+for i in range(21):
+    for j in range(21):
+        mean[i, j] = images[:, i, j].mean()
+        std[i, j] = images[:, i, j].std()
+"""
+"""
 numx, numy = 50, 100
 minx, maxx, miny, maxy = 0, full2theta, minchi, maxchi
 func = gaussian
@@ -32,3 +87,4 @@ for i in range(numx):
         z[i, j] = func(x[i,j], y[i,j])
 
 surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.jet)
+"""
