cabeae76f19445feab80ab0c4c552a25fe6f1daf
volcanozhang <fzhang.cristal@icloud.com>
2013-04-14 17:50:36 +0200

    Correct some errors.

--- write_pts.py
+++ write_pts.py
@@ -1,4 +1,4 @@
 f = open('XY.pts', 'w')
 for i in range(100):
-    f.write('\n %i %i %06f %06f'%(i+1, 0, 15.5, 15.5 + 31.5 * i))
+    f.write('\n %i %i %06f %06f'%(i+1, 0, 12.5 + 31 * i, -12.5))
 f.close()
