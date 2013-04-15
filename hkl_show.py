2f0e4829a231798a7fde371614849becd8b28966
volcanozhang <fzhang.cristal@icloud.com>
2013-04-15 23:10:40 +0200

    Add part which obtain noise parameter.

--- hkl_show.py
+++ hkl_show.py
@@ -32,7 +32,7 @@ def show(hkl, rx = 10, ry = 10):
     plt.show()
     return subimage
 
-def mean_var(hkl, rx = 10, ry = 10):
+def mean_var_hkl(hkl, rx = 10, ry = 10):
     cen = hklxy[hkl]
     int_x, int_y = int(round(cen[1])), int(round(cen[0]))
 
@@ -57,7 +57,32 @@ def mean_var(hkl, rx = 10, ry = 10):
         mean[i], std[i] = subimages[:,i].mean(), subimages[:,i].std()
     var = std**2
     return mean, var
-mean, var = mean_var((-5,-5,-7),rx=7,ry=7)
+def mean_var_xy(xy, rx = 10, ry = 10):
+    int_x, int_y = int(round(xy[0])), int(round(xy[1]))
+
+    xys = []
+    for i in range(int_x-rx, int_x+rx+1):
+        for j in range(int_y-ry, int_y+ry+1):
+            xys.append((i, j))
+    xys = tuple(xys)
+
+    subimages = np.zeros((100, len(xys)))
+    dirpath = '/home/fengguo/Data/Si1g_5N/nomvt/'
+    for i in range(100):
+        path = dirpath + 'S1gnomvt_%s_mar.tiff'%stringint(i,4)
+        f = open(path, 'rb')
+        f.seek(offset)
+        image = np.fromfile(f, dtype = formatdata, count = nb_elem).reshape(framedim)
+        for j in range(len(xys)):
+            subimages[i,j] = image[xys[j]]+bkg[xys[j]]-100
+        f.close()
+    mean, std = np.zeros(len(xys)), np.zeros(len(xys))
+    for i in range(len(xys)):
+        mean[i], std[i] = subimages[:,i].mean(), subimages[:,i].std()
+    var = std**2
+    return mean, var
+"""
+mean, var = mean_var_hkl((-5,-5,-7),rx=7,ry=7)
 k, b = linregress(mean, var)[0:2]
 fig, ax = plt.subplots(1)
 plt.axis([0, mean.max()+1, 0, var.max()+1])
@@ -70,4 +95,32 @@ plt.plot(t, s, 'r')
 textstr = 'slope$=\\alpha$\ninterception with X axis$=\mu-\sigma^2/\\alpha$'
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+"""
+"""
+res = []
+for key in hklxy.keys():
+    mean, var = mean_var_hkl(key, rx=7, ry=7)
+    k, b = linregress(mean, var)[0:2]
+    res.append([1/k, -b/k])
+res = np.array(res)
+fig, ax = plt.subplots(1)
+ax.set_xlabel('$\\frac{1}{\\alpha}$')
+ax.set_ylabel('$\mu-\\frac{\sigma^2}{\\alpha}$')
+plt.plot(res[:,0], res[:,1], '.')
+"""
+res = []
+peaks = np.load('peaks.npy')[2]
+for i in range(peaks.shape[0]):
+    mean, var = mean_var_xy((peaks[i, 1], peaks[i, 0]), rx=7, ry=7)
+    k, b = linregress(mean, var)[0:2]
+    res.append([1/k, -b/k])
+res = np.array(res)
+fig, ax = plt.subplots(1)
+ax.set_xlabel('$\\frac{1}{\\alpha}$')
+ax.set_ylabel('$\mu-\\frac{\sigma^2}{\\alpha}$')
+plt.plot(res[:,0], res[:,1], '.')
+k, b = linregress(res[:,0], res[:,1])[0:2]
+t = np.arange(res[:,0].min()-1, res[:,0].max()+1, (res[:,0].max()-res[:,0].min()+2)/10)
+s = k * t + b
+plt.plot(t, s, 'r')
 plt.show()
