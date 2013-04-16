6886e93f5f4c2678ba767088220cf579438d2e94
volcanozhang <fzhang.cristal@icloud.com>
2013-04-16 11:55:19 +0200

    Add part to generate array of points.

--- write_pts.py
+++ write_pts.py
@@ -1,4 +1,11 @@
+"""
 f = open('XY.pts', 'w')
 for i in range(100):
-    f.write('\n %i %i %06f %06f'%(i+1, 0, 12.5 + 31 * i, -12.5))
+    f.write('\n %i %i %06f %06f'%(i+1, 0, 12 + 31 * i, -12))
+f.close()
+"""
+f = open('XY100x100.pts', 'w')
+for i in range(101):
+    for j in range(101):
+        f.write('\n %i %i %06f %06f'%(i*100+j+1, 0, 12.5 + 25 * j, -12.5 - 25 * i))
 f.close()
