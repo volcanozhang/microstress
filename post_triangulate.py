e658a4588ab84c670f26f84667b3cda4ed818477
volcanozhang <fzhang.cristal@icloud.com>
2013-04-27 22:41:58 +0200

    Add part for initial estimation.

--- post_triangulate.py
+++ post_triangulate.py
@@ -24,6 +24,13 @@ num = pts.shape[0]
 #sy = linregress(stp[0,:,1], stp[1,:,1])[0]
 #sx1 = linregress(stp[1,:,0], stp[2,:,0])[0]
 x0, y0, x1, y1 = pts[:,0], pts[:,1], stp[:,0], stp[:,1]
+
+my, mx = np.zeros((y0.shape[0])), np.zeros((x0.shape[0]))
+for i in range(my.shape[0]):
+    my[i] = (x0[i] * y1[i] - x1[i] * y0[i]) / (x0[i] - x1[i])
+    mx[i] = -(y0[i] - y1[i]) / (x0[i] - x1[i])
+xc, yc = linregress(mx, my)[0:2]
+
 plt.axis([0,2800,0,2800])
 for i in range(stp.shape[0]):
     k, b = linregress(np.array([x0[i],x1[i]]), np.array([-y0[i],-y1[i]]))[0:2]
@@ -31,6 +38,8 @@ for i in range(stp.shape[0]):
     s = k*t+b
     plt.plot(t,s)
     plt.plot(np.array([x0[i],x1[i]]), np.array([-y0[i],-y1[i]]),'r.')
+plt.plot(xc, -yc, 'ro')
+plt.show()
 """
 k, b = linregress(np.array([x0[0],x1[0]]), np.array([-y0[0],-y1[0]]))[0:2]
 t = np.arange(0,2800,200)
@@ -38,7 +47,6 @@ s = k*t+b
 plt.plot(t,s)
 plt.plot(np.array([x0[0],x1[0]]), np.array([-y0[0],-y1[0]]),'r.')
 """
-plt.show()
 """
 stps = np.zeros((13, 39, 2))
 for i in range(1,14):
