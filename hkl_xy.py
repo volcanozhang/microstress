ad005c8e6360a790cc97d5625c82a258180a63f5
volcanozhang <fzhang.cristal@icloud.com>
2013-07-16 10:40:13 +0200

    Correct one error, and add the influence of theta when considering the energy.

--- hkl_xy.py
+++ hkl_xy.py
@@ -11,8 +11,9 @@ import matplotlib.cm as cm
 
 path = '/home/fengguo/Data/Si1g_200N/scan/S1gscan_0100_mar.tiff'
 bkg_path = '/home/fengguo/Data/back_5s_0001.tif'
-fit = np.loadtxt('/home/fengguo/Data/Si1g_200N/scan/dat_S1gscan_0100_mar_LT_1.fit', skiprows = 5)
-cor = np.loadtxt('/home/fengguo/Data/Si1g_200N/scan/S1gscan_0100_mar_LT_1.dat', skiprows = 1)
+fit = np.loadtxt('/home/fengguo/Data/Si1g_200N/scan/dat_S1gscan_0100_mar_LT_0.fit', skiprows = 5)
+dat = np.loadtxt('/home/fengguo/Data/Si1g_200N/scan/S1gscan_0100_mar_LT_0.dat', skiprows = 1)
+cor = np.loadtxt('/home/fengguo/Data/Si1g_200N/scan/dat_S1gscan_0100_mar_LT_0.cor', skiprows = 1)
 f = open(path, 'rb')
 f.seek(offset)
 bkg_f = open(bkg_path, 'rb')
@@ -22,10 +23,14 @@ bkg = np.fromfile(bkg_f, dtype = formatdata, count = nb_elem).reshape(framedim)
 raw_image = image+bkg-100
 
 hkl_xy = dict()
+hkl_theta = dict()
+theta = cor[:,0]/2
 for i in range(fit.shape[0]):
-    xy = cor[fit[i, 0], 2: 4]
+    xy = dat[fit[i, 0], 0: 2]
     hkl_xy[(int(fit[i, 2]), int(fit[i, 3]), int(fit[i, 4]))] = tuple(xy.tolist())
+    hkl_theta[(int(fit[i, 2]), int(fit[i, 3]), int(fit[i, 4]))] = theta[i]
 for key in hkl_xy.keys():
-    plt.text(hkl_xy[key][0],hkl_xy[key][1], '(%i%i%i)'%key)
+    #plt.text(hkl_xy[key][0],hkl_xy[key][1], '(%i%i%i)'%key)
+    plt.text(hkl_xy[key][0],hkl_xy[key][1], '%f'%(np.sin(hkl_theta[key])*np.sqrt(key[0]*key[0]+key[1]*key[1]+key[2]*key[2])))
 plt.imshow(raw_image)
 plt.show()
